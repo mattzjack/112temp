77dd2c5a0730a85e2701cc2a6c1fd70ac92cf06e
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2017-07-25 16:51:37 -0400

    Create rec4.2.py

---
+++ rec4.2.py
@@ -0,0 +1,22 @@
+s0 = set()
+s1 = set()
+x = None
+len(s0)
+x in s0
+x not in s0
+s0.isdisjoint(s1)
+s0.issubset(s1)
+s0 <= s1
+s0 < s1
+s1.issuperset(s0)
+s1 >= s0
+s1 > s0
+s0.union(s1)
+s0 | s1
+s0.intersection(s1)
+s0 & s1
+s0.difference(s1)
+s0 - s1
+s0.symmetric_difference(s1)
+s0 ^ s1
+s0.copy()
